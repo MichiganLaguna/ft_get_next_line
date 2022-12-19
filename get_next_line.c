0296811f60cdc890b1dfa134bc8747f810d49343
nriviere <nriviere@student.42.fr>
2022-12-19 17:36:24 +0100

    f

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: nriviere <nriviere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/17 10:52:02 by nriviere          #+#    #+#             */
-/*   Updated: 2022/12/17 17:38:33 by nriviere         ###   ########.fr       */
+/*   Updated: 2022/12/19 17:35:47 by nriviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,6 @@ int	ft_check_endl(char *str)
 	return (-1);
 }
 
-
 char	*get_next_line(int fd)
 {
 	static char	*fd_lines[MAXFD];
@@ -59,37 +58,30 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || fd > MAXFD)
 		return (0);
+	i[4] = ft_check_endl(fd_lines[fd]);
+	if (i[4] != -1)
+		return (ft_strncut(&(fd_lines[fd]), i[4] + 1));
+	i[0] = 1;
+	i[1] = 0;
+	i[2] = 1;
+	fd_lines[fd] = malloc(1);
+	fd_lines[fd][0] = 0;
 	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buffer)
-		return (buffer);
-	i[0] = ft_check_endl(fd_lines[fd]);
-	i[1] = 1;
-	i[2] = 0;
-	i[3] = 1;
-	while (i[3] && i[0] == -1)
+		return (free(out), buffer);
+	while (i[0] > 0 && i[4] != -1)
 	{
-		i[3] = read(fd, buffer, BUFFER_SIZE);
-		if (i[3] == -1)
-			return (free(buffer), free(fd_lines[fd]), (char *)0);
-		if (i[3] == 0)
-			return (free(buffer), ft_strncut(&(fd_lines[fd]),
-					ft_check_endl(fd_lines[fd]) + 1));
-		if (i[2] < i[3])
+		i[0] = read(fd, buffer, BUFFER_SIZE);
+		buffer[i[0]] = 0;
+		if (i[0] < i[1])
 		{
-			while (i[2] < i[3])
-			{
-				i[1] *= 2;
-				i[2] += i[1] / 2;
-			}
-			fd_lines[fd] = ft_realloc(&(fd_lines[fd]), i[1]);
+			i[2] *= 2;
+			i[1] += i[2] / 2;
+			fd_lines[fd] = ft_realloc(&(fd_lines[fd]),i[2]);
 		}
 		ft_strcat(fd_lines[fd], buffer);
-		i[0] = ft_check_endl(fd_lines[fd]);
-		i[2] -= i[3];
 	}
-	free(buffer);
-	return (ft_strncut(&(*fd_lines),
-			ft_check_endl(*fd_lines)));
+	return (0);
 }
 
 int	main(void)
@@ -101,16 +93,5 @@ int	main(void)
 	fd = open("15.yxt", O_RDONLY);
 	out = get_next_line(fd);
 	printf(":%s:", out);
-	// free(out);
-	
-	// out = get_next_line(fd);
-	// free(out);
-	// out = malloc(10);
-	// out[0] = 0;
-	// printf(":%s:", out);
-	// str = ft_strdup("Hello", 5);
-	// printf(":%s:", str);
-	// ft_strcat(out, str);
-	// printf(":%s:", out);
 	return (0);
 }
